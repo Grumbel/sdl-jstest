90d09c69e581ab270cd1f9285880df71d21e9481
Ingo Ruhnke <grumbel@gmail.com>
2023-07-04 23:02:33 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
@@ -23,42 +23,50 @@
           sdl-jstest = pkgs.stdenv.mkDerivation {
             pname = "sdl-jstest";
             version = "0.0.0";
+
             src = nixpkgs.lib.cleanSource ./.;
+
             patchPhase = ''
               substituteInPlace CMakeLists.txt \
                  --replace SDL_GameControllerDB/gamecontrollerdb.txt '${sdl_gamecontrollerdb}/gamecontrollerdb.txt'
             '';
-            nativeBuildInputs = [
-              pkgs.cmake
-              pkgs.pkgconfig
-            ];
-            buildInputs = [
-              tinycmmc.packages.${system}.default
 
-              pkgs.SDL
-              pkgs.SDL2
-              pkgs.ncurses
+            nativeBuildInputs = with pkgs; [
+              cmake
+              pkgconfig
+            ];
+
+            buildInputs = with pkgs; [
+              SDL
+              SDL2
+              ncurses
+            ] ++ [
+              tinycmmc.packages.${system}.default
             ];
            };
         }) // {
           i686-w64-mingw32-sdl-jstest = pkgs.stdenv.mkDerivation {
             pname = "sdl-jstest";
             version = "0.0.0";
+
             src = nixpkgs.lib.cleanSource ./.;
+
             patchPhase = ''
                 substituteInPlace CMakeLists.txt \
                   --replace SDL_GameControllerDB/gamecontrollerdb.txt '${sdl_gamecontrollerdb}/gamecontrollerdb.txt'
             '';
-            nativeBuildInputs = [
-              pkgs.cmake
-              pkgs.pkgconfig
-            ];
-            buildInputs = [
-              tinycmmc.packages.${system}.default
 
+            nativeBuildInputs = with pkgs; [
+              cmake
+              pkgconfig
+            ];
+
+            buildInputs = with pkgs; [
               # pkgs_mingw32.SDL
               pkgs_mingw32.SDL2
               pkgs_mingw32.ncurses
+            ] ++ [
+              tinycmmc.packages.${system}.default
             ];
           };
         };
